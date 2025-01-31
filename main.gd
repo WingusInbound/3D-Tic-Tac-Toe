cc3756bf585ec1a7238b08dc6804d05f3a30426e
Joe <wingusinbound@gmail.com>
2025-01-30 22:31:03 -0500

    Update main.gd

--- main.gd
+++ main.gd
@@ -4,14 +4,17 @@ extends Node
 @onready var layer_two = $LayerTwo
 @onready var layer_three = $LayerThree
 @onready var layer_four = $LayerFour
+@onready var anim_player = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	layer_one.move(-8,1)
-	layer_two.move(-3,1)
-	layer_three.move(3,1)
-	layer_four.move(8,1)
+	#layer_one.move(-8,1)
+	#layer_two.move(-3,1)
+	#layer_three.move(3,1)
+	#layer_four.move(8,1)
+	anim_player.play("layer_four_anim")
+	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
