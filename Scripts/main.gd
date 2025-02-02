e1e6b59c50ceaad8e6d135cc7626a868bfdc7b83
Canahedo <Canahedo@gmail.com>
2025-02-02 15:50:07 -0500

    Cardinal and diagonal win conditions

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,24 +1,30 @@
 extends Node3D
 
-enum {PLAYER_ONE, PLAYER_TWO}  
+enum {PLAYER_ONE, PLAYER_TWO}
+
+const CUBE_SIZE: int = 4
+
+@export var player_color: Color
+@export var ai_color: Color
+
+# Public
+var game_over = false
+var current_player: Player
+
+# Private
+var square_map = {}
+var possible_wins: Array = []
+var current_turn: int
+var players: Array
+var winner
 
 @onready var layer_one = $LayerOne
 @onready var layer_two = $LayerTwo
 @onready var layer_three = $LayerThree
 @onready var layer_four = $LayerFour
-@onready var anim_player = $AnimationPlayer 
+@onready var anim_player = $AnimationPlayer
 @onready var layers = [layer_one, layer_two, layer_three, layer_four]
 
-@export var player_color: Color
-@export var ai_color: Color
-
-var square_map = {}
-var possible_wins: Array = []
-var current_turn: int
-var current_player: Player
-var players: Array
-var winner
-var game_over = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,40 +39,30 @@ func _ready() -> void:
 	set_square_map(layers)
 	start_game()
 
-func start_game():
-	# Play animation
-	# Set current turn
-	# Set current player
-	current_turn = randi_range(0, 1) 
-	current_player = players[current_turn]
 
-func turn(key):
-	current_turn = (current_turn + 1) % 2
-	current_player = players[current_turn]
-	get_potential_wins(key)
-	winner = check_wins(possible_wins)
-	if winner:
-		if current_turn == 1:
-			print("Player 1 Wins!")
-		else:
-			print("Player 2 Wins")
-		game_over = true
-		anim_player.play_backwards("game_win")
-			
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("spacebar"):
-		anim_player.play("play_game")
-
-
+# Called during Ready, sets up map of cube
 func set_square_map(layers):
 	for i in layers:
 		var children = i.get_children()
 		for child in children:
 			var key = get_string_coords(child.position)
 			square_map.get_or_add(key, child.value)
-	
-	
+
+
+# Called during Ready, chooses and sets starting player
+func start_game():
+	current_turn = randi_range(0, 1)
+	current_player = players[current_turn]
+
+
+# Unpacks cube when Spacebar pushed
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("spacebar"):
+		anim_player.play("play_game")
+
+
+# Triggered when a box is clicked
+# Collects data from selected box and calls for applicable changes
 func tile_selected(square):
 	var key = get_string_coords(square.position)
 	var move = {key: square.value}
@@ -74,6 +70,7 @@ func tile_selected(square):
 	turn(key)
 
 
+# Returns positional cordinates of a box as a 3 digit string
 func get_string_coords(coords: Vector3) -> String:
 	var temp_string: String = ""
 	temp_string += str(coords.x + 2)
@@ -82,38 +79,116 @@ func get_string_coords(coords: Vector3) -> String:
 	return temp_string
 
 
-func get_potential_wins(key):
+# Determines if the selected box results in a win
+# Otherwise, toggle current player
+func turn(key):
 	possible_wins = []
-	get_cardinal_wins(key)
-	#temp_array.append_array(get_diagonal_wins())
-	#temp_array.append_array(get_weird_diagonal_wins())
-	
+	get_potential_wins(key)
+	#get_cross_diagonal_wins()
+	print("Possible wins: ", possible_wins)
+	winner = check_wins(possible_wins)
+	if winner:
+		if current_turn == 0:
+			print("Player 1 Wins!")
+		else:
+			print("Player 2 Wins")
+		game_over = true
+		anim_player.play_backwards("game_win")
+	else:
+		current_turn = (current_turn + 1) % 2
+		current_player = players[current_turn]
 
-func get_cardinal_wins(key):
-	for plane in range(0,3):
-		var box_cord: String
-		var row_list: Array = []
 
-		# Each of the 4 possible values
-		for i in range(0,4):
-			box_cord = ""
+# Creates a list of all possible winstates the key could be in
+func get_potential_wins(key):
+	"""
+	For each of the 3 dimensions, splits the cube into 4 planes
+	The key will be in the same position on each plane, representing a cardinal win through the cube
+	The key may be in either a forward or back diagonal across the plane
+	A cord position is selected to represet the dimension
+	For cardinal wins that position is incremented to represent each plane
+	For diagonal wins that position is locked to check diags across that plane
+	"""
+
+	var cardinal_assembly: String # Assemblies used in finding neighboring boxes when checking wins
+	var diagonal_assembly: String
+	var diag_type: int # 0 = none, 1 = forward, 2 = back
+	var diag_toggle: int = 0 # Used to find the second altered digit on the back diag
+	var card_list: Array = [] # Lists representing a group of 4 neighboring boxes
+	var diag_list: Array = []
+
+	# Marks which cord position represents the relative dimension
+	for dimension in range(0,3):
+		diag_toggle = 0
+		card_list = []
+		diag_list = []
+		# Checks if key is part of a diagonal on this plane
+		diag_type = get_diag_type(dimension, key)
+
+		# Iterates through each possible postional value
+		for i in range(0,CUBE_SIZE):
+			cardinal_assembly = ""
+			diagonal_assembly = ""
 
 			# Represents each position on the cord
 			for cord_digit in range(0,3):
-				# If the position is the one being modified, use i
-				if cord_digit == plane:
-					box_cord += str(i)
-				# Otherwise, use value from original clicked box
-				else:
-					box_cord += str(key[cord_digit])
 
-			# Add cord to row
-			row_list.append(box_cord)
-		# Add row to list of possible wins
-		possible_wins.append(row_list)
-	#print(possible_wins)
-	
-	
+				# If cord position relates to relative dimension
+				if cord_digit == dimension:
+					# Cardinal uses i to represent the plane number
+					cardinal_assembly += str(i)
+					# Diagonal copies the key value for that position
+					diagonal_assembly += key[cord_digit]
+
+				else:
+					# Cardinal copies the key value for that position
+					cardinal_assembly += key[cord_digit]
+					# Forward diag
+					if diag_type == 1:
+						diagonal_assembly += str(i)
+					# Back diag
+					elif diag_type == 2:
+						# First modified digit set to i
+						if diag_toggle == 0:
+							diagonal_assembly += str(i)
+							diag_toggle += 1
+						# Second digit set to max value - i
+						else:
+							diagonal_assembly += str((CUBE_SIZE - 1) - i)
+
+			# Add cords to rows
+			card_list.append(cardinal_assembly)
+			if diag_type != 0:
+				diag_list.append(diagonal_assembly)
+		# Add rows to list of possible wins
+		card_list.append("card")
+		possible_wins.append(card_list)
+		if len(diag_list) > 0:
+			diag_list.append("diag")
+			possible_wins.append(diag_list)
+
+
+func get_diag_type(dimension, key):
+	"""
+	Removes dimentional position from cord to determine if key is part of
+	forward diag (remaining numbers are equal) or
+	back diag (remaining numbers sun to max cube value - 1)
+	"""
+	# Removes dimension from cord to compare remaining digits
+	var local_cords: String = ""
+	for cord_pos in range(0,3):
+		# If cord_pos does not represent the dimension being checked
+		if cord_pos != dimension:
+			# Add the value from key in that position
+			local_cords += key[cord_pos]
+	if int(local_cords[0]) == int(local_cords[1]):
+		return 1 # Forward diag
+	elif int(local_cords[0]) + int(local_cords[1]) == CUBE_SIZE - 1:
+		return 2 # Back diag
+	else:
+		return 0 # No diag on current plane
+
+
 func check_wins(wins):
 	# Checks each row in list of possible wins
 	for row in wins:
@@ -121,11 +196,14 @@ func check_wins(wins):
 
 		# Checks if there are 4 selected boxes in that row
 		for cord in row:
+			if cord in ["diag", "card"]:
+				continue
 			counter += square_map[cord]
 		if abs(counter) == 4:
 			#print("WIN")
 			return row
-			
+
+
 func show_win():
 	#print(winner)
 	for i in layers:
@@ -142,13 +220,13 @@ func show_win():
 				else:
 					child.anim_player.play("shrink_player")
 	anim_player.play("rotate")
-			
-
-class Player: 
-	var color: StandardMaterial3D
-	var value: int
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "game_win":
 		show_win()
+
+
+class Player:
+	var color: StandardMaterial3D
+	var value: int
