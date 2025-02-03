484fce7dbb845fa8d31773ccb48de8731323e178
Joe <wingusinbound@gmail.com>
2025-02-03 14:23:06 -0500

    implemented color picker

--- Scripts/global_vars.gd
+++ Scripts/global_vars.gd
@@ -4,3 +4,5 @@ enum {PLAYER_ONE, PLAYER_TWO}
 enum GameState {START, PLAY, ENDING, DONE}
 
 var cube_size: int = 4
+var player_one_color: Color = Color(255,0,0)
+var player_two_color: Color = Color(0,0,255)
