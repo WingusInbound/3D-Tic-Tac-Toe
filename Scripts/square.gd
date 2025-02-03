c921f74022b65d401fd8ba281d2687e081a92c8a
Joe <76712026+WingusInbound@users.noreply.github.com>
2025-02-03 13:17:45 -0500

    Revert "Adding customized sizes"

--- Scripts/square.gd
+++ Scripts/square.gd
@@ -1,20 +1,15 @@
 extends Node3D
 
-# Public
-var value: int
-
-# Private
-var selected: bool = false
-var main: Node3D
-
-# On Ready
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var main: Node3D = get_owner()
+
+var selected: bool = false
+var wins = []
+var value: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if is_inside_tree():
-		main = get_node("/root/Main")
 	pass # Replace with function body.
 
 
@@ -30,6 +25,7 @@ func _on_area_3d_mouse_entered() -> void:
 		mesh.material_override = main.current_player.color
 
 
+
 func _on_area_3d_mouse_exited() -> void:
 	if selected:
 		return
@@ -42,6 +38,7 @@ func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Ve
 		if selected:
 			return
 		if main.game_state == GlobalVars.GameState.PLAY:
+			#print("Left Clicked")
 			mesh.material_override = main.current_player.color
 			value = main.current_player.value
 			Messenger.SELECTED.emit(self)
