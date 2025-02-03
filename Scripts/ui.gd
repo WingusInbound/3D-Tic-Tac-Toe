9676f7ff1574340012742d5559284affd1bbbc6e
Canahedo <Canahedo@gmail.com>
2025-02-02 21:12:26 -0500

    Created Options Button

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -47,3 +47,7 @@ func _on_resume_button_pressed() -> void:
 
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
